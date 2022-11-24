8e40797209af771ec6bfc1bb68f9a3ffc95891ac
Adriano <adriano_alves_2008@hotmail.com>
2022-11-24 10:01:48 -0300

    estilos iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
+import styles from './App.module.css'
+
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Conteudo...</h1>
+      <main className={styles.main}>
+        <h1>Conteúdo</h1>
+      </main>
       <Footer />
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
-
+import styles from './Footer.module.css'
 
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p><span>React + TS TODO</span> @2022</p>
     </footer>
   )
