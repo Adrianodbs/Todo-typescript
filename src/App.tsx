84b68ec05db9147923248311b98c73a2325b3485
Adriano <adriano_alves_2008@hotmail.com>
2022-11-25 21:50:18 -0300

    preechendo modal com dados

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import {ITask} from './interfaces/Task'
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([])
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null)
 
   const deleteTask = (id: number) =>{
     setTaskList(
@@ -35,18 +36,19 @@ function App() {
     }
   }
 
-  const editTask = ():void => {
+  const editTask = (task: ITask):void => {
     hideOrShowModal(true)
+    setTaskToUpdate(task)
   }
 
   return (
     <div>
-      <Modal children={<TaskForm btnText='Editar tarefa' taskList={taskList}/>} />
+      <Modal children={<TaskForm btnText='Editar tarefa' taskList={taskList} task={taskToUpdate}/>}  />
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText='Criar tarefa' taskList={taskList} setTaskList={setTaskList} />
+          <TaskForm btnText='Criar tarefa' taskList={taskList} setTaskList={setTaskList}  />
         </div>
         <div>
           <h2>Suas Tarefas</h2>
